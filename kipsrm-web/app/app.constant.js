3622a46ff5cffa4cd6e662b5760f834eef9169dc
FaizFizy Rosle <faizfizy@gmail.com>
2017-04-28 17:27:24 +0800

    finish iidf drill down

--- kipsrm-web/app/app.constant.js
+++ kipsrm-web/app/app.constant.js
@@ -5,5 +5,6 @@ app.constant('CONSTANT', {
     ROOT: "kipsrm-web/",
     //BASE_URL: "http://10.1.71.100:9200/kipsrm/",
     BASE_URL: "http://202.45.139.16/kipsrm-es/kipsrm/",
-    BASE_URL_IIDF: "http://10.1.71.100:9500/iidf/"
+    //BASE_URL_IIDF: "http://10.1.71.100:9500/iidf/"
+    BASE_URL_IIDF: "http://202.45.139.16/kipsrm-iidf/"
 });
